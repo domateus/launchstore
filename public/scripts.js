2f0762d044c9bdf65bad267d38af2c44550e9e1e
git <mateusfranceschina@gmail.com>
2020-08-03 08:36:06 -0300

    user log

--- public/scripts.js
+++ public/scripts.js
@@ -14,3 +14,142 @@ const Mask = {
     }
 }
 
+
+const PhotosUpload = {
+    input: "",
+    preview: document.querySelector('#photos-preview'),
+    files: [],
+    uploadLimit: 6,
+    handleFileInput(event) {
+        const { files: fileList } = event.target
+        PhotosUpload.input = event.target
+
+        if (PhotosUpload.hasLimit(event)) return
+
+        Array.from(fileList).forEach(file => {
+
+            PhotosUpload.files.push(file)
+
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image() 
+                image.src = String(reader.result)
+
+                const container = PhotosUpload.getContainer(image)
+
+                PhotosUpload.preview.appendChild(container)
+            }
+
+            reader.readAsDataURL(file)
+        })
+
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+    },
+    hasLimit(event) {
+        const { uploadLimit, input, preview } = PhotosUpload
+        const { files: fileList } = input
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return true
+        }
+
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == "photo")
+                photosDiv.push(item)
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+        if(totalPhotos > uploadLimit) {
+            alert("Você atinfiu o limite de fotos")
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+    getAllFiles() {
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+        
+        return dataTransfer.files
+    },
+    getContainer(image) {
+
+        const container = document.createElement('div')
+        container.classList.add('photo')
+        container.onclick = PhotosUpload.removePhoto
+        container.appendChild(image)
+
+        container.appendChild(PhotosUpload.getRemoveButton())
+
+        return container
+    },
+    getRemoveButton() {
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "close"
+        return button
+    },
+    removePhoto(event) {
+        const photoContainer = event.target.parentNode
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index = photosArray.indexOf(photoContainer)
+
+        PhotosUpload.files.splice(index, 1)
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+        photoContainer.remove()
+    },
+    removeOldPhoto(event) {
+        const photoDiv = event.target.parentNode
+
+        if(photoDiv.id) {
+            const removedFiles = document.querySelector('input[name="removed_files"')
+            if(removedFiles) {
+                removedFiles.value += `${photoDiv.id},`
+            }
+        }
+
+
+
+
+        photoDiv.remove()
+    }
+}
+
+const ImageGallery = {
+    highlight: document.querySelector('.gallery .highlight > img'),
+    previews: document.querySelectorAll('.gallery-preview img'),
+    setImage(e) {
+        const { target } = e
+
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+        target.classList.add('active')
+
+        ImageGallery.highlight.src = target.src
+        Lightbox.image.src = target.src
+    }
+}
+
+const Lightbox = {
+    target: document.querySelector('.lightbox-target'),
+    image: document.querySelector('.lightbox-target img'),
+    closeButton: document.querySelector('.lightbox-target a.lightbox-close'),
+    open() {
+        Lightbox.target.style.opacity = 1
+        Lightbox.target.style.top = 0
+        Lightbox.target.style.bottom = 0
+        Lightbox.closeButton.style.top = 0
+    },
+    close() {
+        Lightbox.target.style.opacity = 0
+        Lightbox.target.style.top = "-100%"
+        Lightbox.target.style.bottom = "initial"
+        Lightbox.closeButton.style.top = "-80px"
+    }
+}
--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,6 +1,7 @@
-const { formatPrice } = require('../lib/utils')
+const { formatPrice, date } = require('../lib/utils')
 const Category = require(`../model/Category`)
 const Product = require(`../model/Product`)
+const File = require(`../model/File`)
 
 module.exports = {
   async create(req, res) {
@@ -22,11 +23,44 @@ module.exports = {
       }
     }
 
+    if (req.files.length == 0)
+      return res.send('Please, send at least one image')
+
     let results = await Product.create(req.body)
     const productId = results.rows[0].id
 
-    return res.redirect(`/products/${productId}`)
+    const filesPromise = req.files.map(file => File.create({...file, product_id: productId
+    }))
 
+    await Promise.all(filesPromise)
+
+    return res.redirect(`/products/${productId}/edit`)
+
+  },
+  async show(req, res) {
+
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    if(!product) return res.send("Product Not Found!")
+
+    const { month, day, hour, minutes } = date(product.updated_at)
+
+    product.published = {
+      day: `${day}/${month}`,
+      hour: `${hour}h${minutes}`
+    }
+
+    product.oldPrice = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
+
+    results = await Product.files(product.id)
+    const files = results.rows.map(file => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`
+    }))
+
+    return res.render("products/show", { product, files })
   },
   async edit(req, res) {
     let results = await Product.find(req.params.id)
@@ -40,15 +74,39 @@ module.exports = {
     results = await Category.all()
     const categories = results.rows
 
-    return res.render("products/edit.njk", { product, categories })
+    results = await Product.files(product.id)
+    let files = results.rows
+    files = files.map(file => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`
+    }))
+
+    return res.render("products/edit.njk", { product, categories, files })
   },
   async put(req, res) {
     const keys = Object.keys(req.body)
 
     for (key of keys) {
-      if (req.body[key == ""]) { 
+      if (req.body[key == "" && key != "removed_files"]) { 
         return res.send('Please, fill all fields!')
       }
+    }
+
+    if (req.files.length != 0) {
+      const newFilesPromise = req.files.map(file => 
+          File.create({...file, product_id: req.body.id}))
+        
+      await Promise.all(newFilesPromise)
+    }
+
+    if (req.body.removed_files) {
+      const removedFiles = req.body.removed_files.split(",")
+      const lastIndex = removedFiles.length - 1
+      removedFiles.splice(lastIndex, 1)
+
+      const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+      await Promise.all(removedFilesPromise)
     }
 
     req.body.price = req.body.price.replace(/\D/g, "")
@@ -60,7 +118,7 @@ module.exports = {
 
     await Product.update(req.body) 
 
-    return res.redirect(`/products/${req.body.id}/edit`)
+    return res.redirect(`/products/${req.body.id}`)
   },
   async delete(req, res) {
     await Product.delete(req.body.id)
